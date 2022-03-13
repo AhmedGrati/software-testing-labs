f1a244e29330e97322f24694012b2c9bba40553c
ahmed <ahmedgrati1999@gmail.com>
2022-03-13 11:53:13 +0100

    fix chronic disease controller and service base test

--- unit-tests/sahti-backend/src/chronic-disease/chronic-disease.controller.spec.ts
+++ unit-tests/sahti-backend/src/chronic-disease/chronic-disease.controller.spec.ts
@@ -2,14 +2,18 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { ChronicDiseaseController } from './chronic-disease.controller';
 import { ChronicDiseaseService } from './chronic-disease.service';
 
-describe('ChronicDiseaseController', () => {
+describe('ChronicDiseaseController Test Suite', () => {
   let controller: ChronicDiseaseController;
+  const mockChronicDiseaseService = {};
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ChronicDiseaseController],
       providers: [ChronicDiseaseService],
-    }).compile();
+    })
+    .overrideProvider(ChronicDiseaseService)
+    .useValue(mockChronicDiseaseService)
+    .compile();
 
     controller = module.get<ChronicDiseaseController>(ChronicDiseaseController);
   });
--- unit-tests/sahti-backend/src/chronic-disease/chronic-disease.service.spec.ts
+++ unit-tests/sahti-backend/src/chronic-disease/chronic-disease.service.spec.ts
@@ -1,18 +1,35 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { ChronicDiseaseService } from './chronic-disease.service';
+import { ChronicDisease } from './entities/chronic-disease.entity';
 
-describe('ChronicDiseaseService', () => {
+describe('ChronicDiseaseService Test Suite', () => {
   let service: ChronicDiseaseService;
-
+  const mockChronicDiseaseRepository = {
+    find: jest.fn().mockImplementation(
+      () => {
+        return [];
+      }
+    )
+  };
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ChronicDiseaseService],
-    }).compile();
+      providers: [ChronicDiseaseService, {
+        provide: getRepositoryToken(ChronicDisease),
+        useValue: mockChronicDiseaseRepository
+      }],
+    })
+    .compile();
 
     service = module.get<ChronicDiseaseService>(ChronicDiseaseService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+  it('should return the list of all chronic diseases', async () => {
+    expect(await service.findAll()).toBeDefined();
+    expect(mockChronicDiseaseRepository.find).toBeCalled();
+    expect(await service.findAll()).toEqual([]);
   });
 });
